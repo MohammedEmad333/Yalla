3a6262743ea0b7bd8fc6ad27a17fd604138021c6
Mohammed Emad <126204744+MohammedEmad333@users.noreply.github.com>
2026-09-16 03:26:33 +0300

    Protect restaurant rating route

--- backend/src/routes/restaurant.routes.js
+++ backend/src/routes/restaurant.routes.js
@@ -6,10 +6,13 @@
 
 const router = require('express').Router();
 const ctrl = require('../controllers/restaurant.controller');
+const { authenticate, authorize } = require('../middlewares/auth.middleware');
+const { ROLES } = require('../utils/constants');
 
 // التصنيفات قبل مسار المعرّف لتفادي التقاط "categories" كمعرّف مطعم
 router.get('/categories', ctrl.listCategories);
 router.get('/', ctrl.listRestaurants);
+router.post('/:restaurantId/rate', authenticate, authorize(ROLES.USER), ctrl.rateRestaurant);
 router.get('/:restaurantId', ctrl.getRestaurant);
 
 module.exports = router;
