835805318116dbe98a2e5202e0ecf662f385b697
Mohammed Emad <126204744+MohammedEmad333@users.noreply.github.com>
2026-09-16 04:17:08 +0300

    feat: add partner staff controllers

---
+++ backend/src/controllers/merchantStaff.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+const service = require('../services/merchantStaff.service');
+
+module.exports = {
+  login: async (req, res, next) => { try { res.json(await service.login(req.body.phone, req.body.password)); } catch (err) { next(err); } },
+  list: async (req, res, next) => { try { res.json(await service.list(req.auth.id)); } catch (err) { next(err); } },
+  create: async (req, res, next) => { try { res.status(201).json(await service.create(req.auth.id, req.auth, req.body)); } catch (err) { next(err); } },
+  update: async (req, res, next) => { try { res.json(await service.update(req.auth.id, req.auth, req.params.staffId, req.body)); } catch (err) { next(err); } },
+  remove: async (req, res, next) => { try { res.json(await service.remove(req.auth.id, req.auth, req.params.staffId)); } catch (err) { next(err); } },
+};
