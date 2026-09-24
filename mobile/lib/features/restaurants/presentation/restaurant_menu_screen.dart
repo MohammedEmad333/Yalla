aab37900406f41c38fcd243b56b88a9367e630ec
Mohammed Emad <126204744+MohammedEmad333@users.noreply.github.com>
2026-09-24 03:14:00 +0300

    chore(mobile): use scrollCacheExtent

--- mobile/lib/features/restaurants/presentation/restaurant_menu_screen.dart
+++ mobile/lib/features/restaurants/presentation/restaurant_menu_screen.dart
@@ -451,7 +451,7 @@ class _RestaurantMenuScreenState extends State<RestaurantMenuScreen> {
 
     return CustomScrollView(
       controller: _scrollController,
-      cacheExtent: 700,
+      scrollCacheExtent: 700,
       physics: const AlwaysScrollableScrollPhysics(),
       slivers: [
         SliverToBoxAdapter(child: KeyedSubtree(key: _heroKey, child: _restaurantHero())),
